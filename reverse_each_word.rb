6d05f8e1fef32f563e04826e43a328474db5eaeb
Diana Wendt <dianakw@gmail.com>
2020-01-14 17:32:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,11 @@
-def reverse_each_word(string)
-  a = string.split(" ").each {|n| n.reverse!}
-  p a
-  a.join(" ")
-end
-
 #def reverse_each_word(string)
-#  b = string.split(" ").each {|n| n.reverse}.join(" ")
- # p b
+#  a = string.split(" ").each {|n| n.reverse!}
+#  p a
+#  a.join(" ")
 #end
+
+def reverse_each_word(string)
+  string.split(" ").each {|n| n.reverse}.join(" ")
+end
 
     
